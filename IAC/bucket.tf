dd298a3740f7624fda522f7651b5e5cd037ccb4d
pd00 <parsadarbouy@gmail.com>
2024-10-03 14:32:08 -0600

    upload files but not folders

--- IAC/bucket.tf
+++ IAC/bucket.tf
@@ -21,3 +21,13 @@ resource "aws_s3_bucket_acl" "static_website" {
   acl    = "private"
 }
 
+resource "aws_s3_object" "static_website" {
+  bucket = aws_s3_bucket.static_website.id
+  for_each = fileset("../Personal-website/","*")
+  key = each.value
+  source = "../Personal-website/${each.value}"
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5("../Personal-website/${each.value}")
+}
